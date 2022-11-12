c35553da521c166d193359a2101641143717c5e4
XTRMJ <111897180+XTRMJ@users.noreply.github.com>
2022-11-12 09:54:48 -0600

    Integracion de animaciónes (Scripts)
    Se añadieron las animaciones faltantes en el script "MovePlayer".
    -Falta mejorar las animaciones como:
    Salto/Apuntado hacia arriba (acoplamiento)
    Salto/Apuntado hacia abajo (acoplamiento)
    Pecho tierra (No se debería mover)
    
    -Se creo "FollowPlayer" para el seguimiento de la camará

--- Assets/Scripts/MovePlayer.cs
+++ Assets/Scripts/MovePlayer.cs
@@ -6,12 +6,16 @@ public class MovePlayer : MonoBehaviour
 {
     private float horizontal;
     public float dir;
+    private bool gameOver;
+    private int damage;
     private SpriteRenderer sr;
     private Animator anim;
     private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
+        damage = 0;
+        gameOver = false;
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         sr = GetComponent<SpriteRenderer>();
@@ -20,39 +24,48 @@ public class MovePlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.UpArrow)){
-            rb.AddForce(Vector2.up * 4, ForceMode2D.Impulse);
-            anim.SetTrigger("isJump");
-        }else if(Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.LeftArrow)){
-            if(Input.GetKey(KeyCode.LeftArrow))
-            {
-                dir = -1;
-                sr.flipX = true;
-            }else{
-                dir = 1;
-                sr.flipX = false;
-            }
-            horizontal = Input.GetAxis("Horizontal");
-            transform.Translate(Vector3.right * Time.deltaTime * horizontal);
-            if(Input.GetKeyDown(KeyCode.Space)){
+        if(!gameOver){
+            if(Input.GetKeyDown(KeyCode.UpArrow)){
+                rb.AddForce(Vector2.up * 4, ForceMode2D.Impulse);
+                anim.SetTrigger("isJump");
+            }else if(Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.LeftArrow)){
+                if(Input.GetKey(KeyCode.LeftArrow))
+                {
+                    dir = -1;
+                    sr.flipX = true;
+                }else{
+                    dir = 1;
+                    sr.flipX = false;
+                }
                 anim.SetBool("isRun",true);
+                if(Input.GetKey(KeyCode.UpArrow)) anim.SetBool("isLookUp",true);
+                else if(Input.GetKey(KeyCode.DownArrow)) anim.SetBool("isLookDown",true);
+                horizontal = Input.GetAxis("Horizontal");
+                transform.Translate(Vector3.right * Time.deltaTime * horizontal);
+                if(Input.GetKeyDown(KeyCode.Space)){
+                    anim.SetTrigger("isFire");
+                };
+            }else if(Input.GetKeyDown(KeyCode.Space))
+            {
                 anim.SetTrigger("isFire");
-            }else anim.SetBool("isRun",true);
-        }else if(Input.GetKeyDown(KeyCode.Space))
-        {
-            anim.SetTrigger("isFire");
-        }else if(Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            if(Input.GetKeyDown(KeyCode.DownArrow))
+            }else if(Input.GetKey(KeyCode.DownArrow))
             {
                 anim.SetBool("isGrounded", true);
             }else{
-
+                anim.SetBool("isRun",false);
+                anim.SetBool("isJump",false);
+                anim.SetBool("isFire",false);
+                anim.SetBool("isLookUp",false);
+                anim.SetBool("isLookDown",false);
+                anim.SetBool("isGrounded",false);
             }
-        }else{
-            anim.SetBool("isRun",false);
-            anim.SetBool("isJump",false);
-            anim.SetBool("isFire",false);
-        }
+        }else anim.SetBool("isDead",true);
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {/*
+        if(col.CompareTag("BulletEnemy"))
+            if(damage < 4) damage++;
+            else gameOver = true;*/
     }
 }
