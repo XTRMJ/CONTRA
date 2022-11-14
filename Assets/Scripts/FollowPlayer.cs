c70b4d59e76db096b575143558c030996bacf0d2
XTRMJ <111897180+XTRMJ@users.noreply.github.com>
2022-11-14 13:37:54 -0600

    Musica y otros
    Se le agrego musica al juego
    Script para el seguimiento de la camara (debe ser mejorado no funciona)

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
 {
+    public GameObject player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GetComponent<GameObject>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = new Vector2(player.transform.position.x,0.25f);
     }
 }
